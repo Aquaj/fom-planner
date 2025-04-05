2ceb438dac13f584bc74b9d0b1e5273d0078e991
Jérémie Bonal <jeremie.bonal@gmail.com>
2025-04-05 13:53:19 +0200

    Extract scrollability logic

--- drawer.ts
+++ drawer.ts
@@ -1,10 +1,12 @@
 import createjs from "createjs-module";
 import Grid from "./grid";
 
+import scrollable from "./scrollable";
+
 class Drawer {
+  rootElement: createjs.Container;
   background: createjs.Shape;
   grid: Grid;
-  rootElement: createjs.Container;
   width: number;
   height: number;
   scroller: (event: WheelEvent) => void;
@@ -13,29 +15,10 @@ class Drawer {
     this.width = width;
     this.height = height;
     this.rootElement = new createjs.Container();
-    this.rootElement.on("mouseover", (event: createjs.Event) => {
-      this.scroller = (event: WheelEvent) => { this.scroll(event) };
-      document.addEventListener("wheel", this.scroller);
-    })
-    this.rootElement.on("mouseout", (event: createjs.Event) => {
-      document.removeEventListener("wheel", this.scroller);
-    })
-  }
+    this.background = new createjs.Shape();
+    this.grid = new Grid(20, 10, this.width - 10, this.height * 2);
 
-  scroll(event: WheelEvent) {
-    const grid_top = this.grid.rootElement.y;
-    const grid_bottom = this.grid.rootElement.y + this.grid.height;
-    if (grid_top >= 0 && event.deltaY > 0) {
-      this.grid.rootElement.y = 0;
-      return
-    }
-    if (grid_bottom <= this.height && event.deltaY < 0) {
-      this.grid.rootElement.y = this.height - this.grid.height;
-      return
-    }
-    if (grid_top <= 0 && grid_bottom >= this.height) {
-      this.grid.rootElement.y += event.deltaY;
-    }
+    scrollable.makeScrollable(this, this.grid, 0, this.height);
   }
 
   draw() {
@@ -44,7 +27,6 @@ class Drawer {
   }
 
   addBackground() {
-    this.background = new createjs.Shape();
     this.background.graphics
       .setStrokeStyle(1)
       .beginStroke("black")
@@ -55,7 +37,6 @@ class Drawer {
   }
 
   addGrid() {
-    this.grid = new Grid(20, 10, this.width - 10, this.height * 2);
     this.grid.draw();
     this.rootElement.addChild(this.grid.rootElement);
     this.rootElement.setChildIndex(this.grid.rootElement, 1);
---
+++ scrollable.ts
@@ -0,0 +1,34 @@
+const scrollable = {
+  makeScrollable: function (container: any, content: any, upperBound: number = 0, lowerBound: number = container.height) {
+
+    const containerElement = container.rootElement;
+    const scroller = (event: WheelEvent) => {
+      this.scroll(event, content, upperBound, lowerBound)
+    };
+    containerElement.on("mouseover", (event: createjs.Event) => {
+      console.log("Adding scroller");
+      document.addEventListener("wheel", scroller);
+    })
+    containerElement.on("mouseout", (event: createjs.Event) => {
+      console.log("Removing scroller");
+      document.removeEventListener("wheel", scroller);
+    })
+  },
+
+  scroll: function (event: WheelEvent, target: any, upperBound: number, lowerBound: number) {
+    const elementTop = target.rootElement.y;
+    const elementBottom = target.rootElement.y + target.height;
+    if (elementTop >= upperBound && event.deltaY > 0) {
+      target.rootElement.y = 0;
+      return
+    }
+    if (elementBottom <= lowerBound && event.deltaY < 0) {
+      target.rootElement.y = lowerBound - target.height;
+      return
+    }
+
+    target.rootElement.y += event.deltaY;
+  }
+}
+
+export default scrollable;
